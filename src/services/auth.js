28c876989fea1328361ab5f2218d0b0a9f20ac4d
elwiiman <emmanuel.sanro@gmail.com>
2019-12-12 20:47:30 -0600

    services route for heroku fixed

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const isProduction = process.env.NODE_ENV === "production";
-const base_url = isProduction ? "heroku.com" : "http://localhost:3000/api";
+const base_url = isProduction ? "https://mexico-para-todos.herokuapp.com/api" : "http://localhost:3000/api";
 
 export const login = data => {
   return axios.post(`${base_url}/login`, data);
--- src/services/travel.js
+++ src/services/travel.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 const isProduction = process.env.NODE_ENV === "production";
-const base_url = isProduction ? "heroku.com" : "http://localhost:3000/api";
+const base_url = isProduction
+  ? "https://mexico-para-todos.herokuapp.com/api"
+  : "http://localhost:3000/api";
 
 export const getTravels = () => {
   const token = localStorage.getItem("token");
